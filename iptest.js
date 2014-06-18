96c28663f099a138c5a9d2d8e0806dfa25ff9f21
Igor Alekseev <mail@igoralekseev.info>
2014-06-18 18:52:35 +0400

    тестирую https через прокси

--- iptest.js
+++ iptest.js
@@ -3,11 +3,14 @@ var request = require('request')
 // var proxy = { url: 'http://213.85.92.10', port: 80 }
 var proxy = {  url: '37.79.254.147', port: 3128 }
 
+var options = JSON.parse(fs.readFileSync('options.json').toString())
+
+
 var ip = 'http://curlmyip.com'
 
 
 // request = request.defaults({'proxy':'http://213.85.92.10'})
-request = request.defaults({'proxy':'http://' + proxy.url + ':' + proxy.port })
+request = request.defaults({'proxy': options.proxy })
 request(ip, function (error, response, body) {
 	console.log(body, error)
 })
@@ -18,8 +21,8 @@ request(ip, function (error, response, body) {
 var http = require('http');
 
 http.get({
-    host: proxy.url,
-    port: proxy.port,
+    host: options.proxy.split(':')[0],
+    port: options.proxy.split(':')[1],
     path: ip
 }, function (response) {
     // console.log (response);
@@ -27,3 +30,58 @@ http.get({
     	console.log('BODY: ' + chunk);
   	});
 });
+
+
+
+test_vk = function(_method, _params, token) {
+
+        // var options = {
+        //     host: 'api.vk.com',
+        //     port: 443,
+        //     path: '/method/' + _method + '?' +
+        //         'access_token=' + token
+        // };
+
+
+		var port = {
+			host: options.proxy.split(':')[0],
+    		port: options.proxy.split(':')[1],
+    		path: 'https://api.vk.com/method/' + _method + '?' + 'access_token=' + token
+		}
+
+        for(var key in _params) {
+            if( key === "message" ) {
+                options.path += ('&' + key + '=' + encodeURIComponent(_params[key]));
+            } else {
+                options.path += ('&' + key + '=' + _params[key]);
+            }
+        }
+
+        https.get(options, function(res) {
+            var apiResponse = new String();
+            res.setEncoding('utf8');
+
+            res.on('data', function(chunk) {
+                apiResponse += chunk;
+            });
+
+            res.on('end',  function() {
+                var o = JSON.parse(apiResponse);
+                console.log('vk done:' + _method, o);
+            });
+
+        });
+    };
+
+
+
+var options = JSON.parse(fs.readFileSync('options.json').toString())
+var token = JSON.parse(fs.readFileSync('token.json').toString())
+
+test_vk('messages.get', { count: 20, v: '5.21' }, token.value);
+
+
+
+
+
+
